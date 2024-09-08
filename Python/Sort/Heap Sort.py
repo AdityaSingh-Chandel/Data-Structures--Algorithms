52ba4b18d36fece9313c4028e3b7b05d7930e3c6
AdityaSingh <adityaapssinghcs@gmail.com>
2024-09-08 12:23:40 +0530

    Update Heap Sort.py

--- Python/Sort/Heap Sort.py
+++ Python/Sort/Heap Sort.py
@@ -1,46 +1,9 @@
-'''
-def buildHeap(arr):
-    n = len(arr)
-    # Build a max heap by heapifying from the bottom up
-    for i in range((n - 2) // 2, -1, -1):
-        maxHeapify(arr, n, i)
-    
-def maxHeapify(arr, n, i):
-    largest = i
-    left = 2 * i + 1
-    right = 2 * i + 2
-    # Compare left child
-    if left < n and arr[left] > arr[largest]:
-        largest = left
-    # Compare right child
-    if right < n and arr[right] > arr[largest]:
-        largest = right
-    # Swap and continue heapifying if root is not the largest
-    if largest != i:
-        arr[i], arr[largest] = arr[largest], arr[i]
-        maxHeapify(arr, n, largest)
-
-def heapSort(arr):
-    n = len(arr)
-    # Step 1: Build a max heap
-    buildHeap(arr)
-    print("Initial max heap:", arr)
-    # Step 2: Extract elements from the heap one by one
-    for i in range(n - 1, 0, -1):
-        # Move current root to the end
-        arr[0], arr[i] = arr[i], arr[0]
-        # Heapify the reduced heap
-        maxHeapify(arr, i, 0)
-        print("i:", i, "arr:", arr)
-    return arr
-
-'''
+                                    # 08-09-24
 def buildHeap(arr):
     n=len(arr)
     for i in range((n-2)//2,-1,-1):
         maxHeapify(arr,n,i)
-    
-        
+           
 def maxHeapify(arr,n,i):
     largest=i
     left=2*i+1
@@ -57,13 +20,15 @@ def maxHeapify(arr,n,i):
 def heapSort(arr):
     n=len(arr)
     buildHeap(arr)
-    print(arr)
+    #print(arr)
     for i in range(n-1,0,-1):
         arr[0],arr[i]=arr[i],arr[0]
         maxHeapify(arr,i,0) 
-        print("i:",i,"arr:",arr)
+        #print("i:",i,"arr:",arr)
     return arr 
 
+
+# Driver Code
 
 arr=[78,23,4,67,8,34,12]  
 #print(buildHeap(arr))  
